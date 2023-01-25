905e3924451431e3f940dfc1ba3dbe287228b765
Othon <programmingxsup@gmail.com>
2023-01-25 15:03:05 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+using Reflection;
+
+void ListInObject<T>(T x)
+{
+  var type = x.GetType();
+
+  Console.WriteLine(string.Join("\n", type.GetRuntimeProperties()));
+  Console.WriteLine(string.Join("\n", type.GetRuntimeMethods()));
+}
+
+T Instantiate<T>()
+  => Activator.CreateInstance<T>();
+
+var console = new Videogame()
+{
+  Id = 123,
+  Name = "PS5",
+  Fabricante = "Sony",
+  Nota = 9
+};
+
+var num = 1;
+
+ListInObject(console);
+Console.WriteLine("\n------------------\n");
+ListInObject(num);
+Console.WriteLine("\n------------------\n");
+
+var console2 = Instantiate<Videogame>();
+Console.WriteLine(console2);
---
+++ Videogame.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection
+{
+  internal class Videogame
+  {
+    public long Id { get; set; }
+    public string Name { get; set; }
+    public string Fabricante { get; set; }
+    public int Nota { get; set; }
+
+    private void Fabricar()
+      => Console.WriteLine($"{Name} fabricado");
+
+    public void Vender()
+      => Console.WriteLine($"{Name} vendido com sucesso");
+  }
+}
